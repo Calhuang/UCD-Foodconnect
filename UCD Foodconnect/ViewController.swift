8539e4fdf9e1945cd6449242612b92ff2b7f44d2
Calvin Huang <clvnln794@gmail.com>
2018-04-30 11:12:01 -0700

    Initial Commit

---
+++ UCD Foodconnect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCD Foodconnect
+//
+//  Created by Calvin on 4/30/18.
+//  Copyright © 2018 Calvin Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
